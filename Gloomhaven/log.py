276f3b2b1996ca620181f70f18e398c2a07298c2
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2024-10-17 20:31:10 -0400

    Make adding observer to log simpler

--- Gloomhaven/log.py
+++ Gloomhaven/log.py
@@ -3,10 +3,14 @@ from observable import Observable
 
 class Log(Observable):
     def __init__(self):
+        self.observers = {"log": []}
         self.log = []
 
     def __repr__(self):
         print(self.log)
+
+    def add_observer(self, method):
+        return super().add_observer("log", method)
 
     def add(self, string_to_log):
         self.log.append(string_to_log)
