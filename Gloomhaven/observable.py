0cb9082c9678de989f17777358e083830a87e665
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2024-10-18 22:36:48 -0400

    Rename event to key

--- Gloomhaven/observable.py
+++ Gloomhaven/observable.py
@@ -2,12 +2,11 @@ class Observable:
     def __init__(self):
         self.observers = {}
 
-    def add_observer(self, event: str, method):
-        if event not in self.observers:
-            self.observers[event] = []
-        self.observers[event].append(method)
-        print(self.observers)
+    def add_observer(self, key: str, method):
+        if key not in self.observers:
+            self.observers[key] = []
+        self.observers[key].append(method)
 
-    def notify_observers(self, event, data):
-        for method in self.observers[event]:
+    def notify_observers(self, key, data):
+        for method in self.observers[key]:
             method(data)
